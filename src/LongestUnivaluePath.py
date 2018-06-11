959084d5ba1d7f0ba294bdd20747c541c00c6c91
jostalker1 <myxin@bk.ru>
2018-06-11 08:27:29 +0300

    added new tasks

--- src/LongestUnivaluePath.py
+++ src/LongestUnivaluePath.py
@@ -4,62 +4,39 @@ class TreeNode:
         self.left = None
         self.right = None
 
-
 class Solution:
-    def cons_or_snoc(self, queue, node, cur_value):
-        if not node:
-            return
-        if node.val == cur_value:
-            queue.insert(0, node)
-        else:
-            queue.append(node)
-
     def longestUnivaluePath(self, root):
         if not root:
             return 0
-        queue = []
-        queue.append(root)
-        max_len = 1
-        cur_len = 1
-        cur_value = -1
-        while len(queue) > 0:
-            node = queue.pop(0)
-
-            if cur_value == node.val:
+        nodes = [root]
+        max_len = 0
+        while len(nodes) > 0:
+            same_nodes = [nodes.pop(0)]
+            cur_len = 0
+            while len(same_nodes) > 0:
+                node = same_nodes.pop(0)
                 cur_len += 1
-            else:
-                if cur_len > max_len:
-                    max_len = cur_len
-                cur_value = node.val
-                cur_len = 1
+                if node.left:
+                    if node.left.val == node.val:
+                        same_nodes.append(node.left)
+                    else:
+                        nodes.append(node.left)
+                if node.right:
+                    if node.right.val == node.val:
+                        same_nodes.append(node.right)
+                    else:
+                        nodes.append(node.right)
+            if cur_len > max_len:
+                max_len = cur_len
 
-
-            if node.left and node.right and node.left.val == node.right.val:
-                queue.insert(0, node.right)
-                queue.insert(0, node.left)
-            else:
-                self.cons_or_snoc(queue, node.left, cur_value)
-                self.cons_or_snoc(queue, node.right, cur_value)
-
-
-        return (max_len - 1) if cur_len < max_len else (cur_len - 1)
-
+        return max_len - 1
 
 sol = Solution()
-root = TreeNode(5)
+root = TreeNode(1)
 root.left = TreeNode(4)
 root.right = TreeNode(5)
-root.left.left = TreeNode(1)
-root.left.right = TreeNode(1)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(4)
 root.right.right = TreeNode(5)
 print(sol.longestUnivaluePath(root))
 
-root= TreeNode(1)
-root.left = TreeNode(1)
-root.right = TreeNode(1)
-print(sol.longestUnivaluePath(root))
-
-root= TreeNode(2)
-root.left = TreeNode(1)
-root.right = TreeNode(1)
-print(sol.longestUnivaluePath(root))
