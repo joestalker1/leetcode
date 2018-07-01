33bd20127b1a8e5ee7caa6a824c99c056a4c63e1
jostalker1 <myxin@bk.ru>
2018-07-01 20:43:28 +0300

    added new tasks

---
+++ src/ReverseString.scala
@@ -0,0 +1,15 @@
+trait ReverseString {
+}
+
+object ReverseStringApp extends ReverseString with App{
+   println(reverseString("abc"))
+}  def reverseString(s: String): String = {
+  if (s == null || s.isEmpty) s
+  else {
+    var stack = List.empty[Char]
+    for (ch <- s) stack = ch :: stack
+    val sbf = new StringBuilder
+    for (ch <- stack) sbf.append(ch)
+    sbf.result()
+  }
+}
