3228e21ca96451b00b4a34c7eb5202a685f0b95a
joestalker1 <denys.fomenko@xenoss.io>
2019-01-16 00:14:45 +0200

    Added new assigments

--- src/main/scala/EvaluateDivision.py
+++ src/main/scala/EvaluateDivision.py
@@ -1,44 +1,19 @@
-# class UnionFind:
-#     def __init__(self, equations):
-#         self.parent = [-1] * len(equations)
-#         self.rank = [0] * len(equations)
-#         for i in range(len(self.parent)):
-#             self.parent[i] = i
-#
-#     def find(self, i):
-#         if self.parent[i] != i:
-#             self.parent[i] = self.parent(self.parent[i])
-#         return self.parent
-#
+from collections import defaultdict
 
 
 class Solution:
-    def path(self, adj, s, n, e, buf):
-        buf.append(s)
+    def dfs(self, adj, s, e, r, seen):
+        if s not in adj.keys() or s in seen:
+            return -1
+        seen.add(s)
         if s == e:
-            return buf
-        res = None
-        for nodes in adj[s]:
-            if nodes[0] == n:
-                continue
-            bufc = buf[:]
-            resc = self.path(adj, nodes[0], s, e, bufc)
-            if not res and resc:
-                res = resc
-            elif resc and len(resc) < len(res):
-                res = resc
-        return res
-
-    def eval_weight(self, adj, nodes):
-        w = 1
-        for i in range(len(nodes) - 1):
-            a = nodes[i]
-            b = nodes[i + 1]
-            for n in adj[a]:
-                if n[0] == b:
-                    w *= n[1]
-                    break
-        return w
+            return r
+        for p in adj[s]:
+            n, w = p
+            r1 = self.dfs(adj, n, e, r * w, seen)
+            if r1 != -1:
+                return r1
+        return r1
 
     def calcEquation(self, equations, values, queries):
         """
@@ -49,41 +24,28 @@ class Solution:
         """
         if not equations:
             return []
-        adj = {}
+        adj = defaultdict(list)
         for i in range(len(equations)):
-            pair = equations[i]
-            if pair[0] not in adj.keys():
-                adj[pair[0]] = [[pair[1], values[i]]]
-            else:
-                adj[pair[0]].append([pair[1], values[i]])
-            if pair[1] not in adj.keys():
-                adj[pair[1]] = [[pair[0], 1 / values[i]]]
-            else:
-                adj[pair[1]].append([pair[0], 1 / values[i]])
-        res = []
+            p0, p1 = equations[i]
+            adj[p0].append([p1, values[i]])
+            adj[p1].append([p0, 1 / values[i]])
+        res = list()
         for query in queries:
-            s = query[0]
-            e = query[1]
-            if s not in adj.keys() or e not in adj.keys():
-                res.append(-1)
-            elif s == e:
-                res.append(1)
-            else:
-                nodes = self.path(adj, s,"", e, [])
-                res.append(self.eval_weight(adj, nodes))
+            s, e = query
+            res.append(self.dfs(adj, s, e, 1, set()))
         return res
+
 
 sol = Solution()
 
-eq = [["a","e"],["b","e"]]
-vl = [4.0,3.0]
-qr = [["a","b"],["e","e"],["x","x"]]
-print(sol.calcEquation(eq, vl, qr))
-
+# eq = [["a", "e"], ["b", "e"]]
+# vl = [4.0, 3.0]
+# qr = [["a", "b"], ["e", "e"], ["x", "x"]]
+# print(sol.calcEquation(eq, vl, qr))
 
 equestions = [["a", "b"], ["b", "c"]]
-values=[2.0, 3.0]
-queries1 = [ ["a","c"],["b","c"],["a","e"],["a","a"],["x","x"] ]
-queries2 = [ ["a", "e"],["a", "c"], ["b", "a"], ["a", "a"], ["x", "x"] ]
+values = [2.0, 3.0]
+queries1 = [["a", "c"], ["b", "c"], ["a", "e"], ["a", "a"], ["x", "x"]]
+queries2 = [["a", "e"], ["a", "c"], ["b", "a"], ["a", "a"], ["x", "x"]]
 
 print(sol.calcEquation(equestions, values, queries1))
